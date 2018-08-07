ea6d0f034baf80c965939f17a3a0d6fb821d76f1
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-08-07 01:28:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def sort_array_asc(array)
   array.sort
 end
@@ -7,5 +9,40 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort {|x,y| x<=> y}
+  array.sort_by(&:length)
 end
+
+def swap_elements(array)
+  element = array.slice!(1,1)
+  element = element.join(" ")
+  array[2] = element
+  return array 
+end
+
+def swap_elements_from_to(array,index,destination_index)
+  element = array.slice!(index,1)
+  element = element.join(" ")
+  array[destination_index] = element
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+  return array
+end
+
+def add_s(array)
+  array.each_with_index.collect do  |element,index|
+    if !(index == 1) 
+      element << "s"
+    else 
+      array << element
+    end
+  end
+end
+
