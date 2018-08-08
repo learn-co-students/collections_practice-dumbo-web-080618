98e1ce99f62644c3432bbf9f3aee852622767994
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-08 16:30:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+require "pry"
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  sort_array_asc(array).reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  if array.length > 3
+    array = array[0], array[2], array[1], array[3..-1]
+  elsif array.length == 3 
+    array =  array[0], array[2], array[1]
+  else
+    nil 
+  end
+end
+    
+def reverse_array(array)
+  array = array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+  
+def sum_array(array)
+  array.reduce(:+)
+end
+  
+def add_s(array)
+  array.each do |word|
+    if !(word = array[2])
+      word << "s"
+      p word
+    end
+  end
+end
+      
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
